b1062960b41b1e7aba9496a20b1d669a61f10163
keyra5184 <52947467+keyra5184@users.noreply.github.com>
2019-08-05 02:57:37 -0600

    Add files via upload

---
+++ tarea1.java
@@ -0,0 +1,45 @@
+
+package tar3;
+
+/*
+ * @author Keyra Lima
+ */
+public class tarea1 {
+    
+    public void ejercicio1(){
+        System.out.println("Keyra Lima");
+    }
+    
+    public void ejercicio2(){
+    System.out.print("Keyra Lima\nDirección: San José Acatempa, Jut\nTel: 49728791");
+    }
+    
+    public void ejercicio3(){
+        System.out.print("1.Hello\tHola\n");
+        System.out.print("2.Dog  \tPerro\n");
+        System.out.print("3.House\tCasa\n");
+        System.out.print("4.Water\tAgua \n");
+        System.out.print("5.Lose \tPerder\n");
+        System.out.print("6.Dream\tSueño\n");
+        System.out.print("7.Can \tPoder\n");
+        System.out.print("8.Good\tBueno\n");
+        System.out.print("9.God \tDios \n");
+        System.out.print("10.Cat\tGato \n");
+    }
+    
+    public void ejercicio4(){
+        System.out.println("Microeconomía \n 7am- 9am");
+        System.out.println("Programación  \n 9am- 11am");
+        System.out.println("Calculo II    \n11am-1pm");
+        System.out.println("Estadistica   \n 2pm-4pm");
+        System.out.println("Física II     \n 4pm-6pm");
+    }
+    
+    public void ejercicio5(){
+        System.out.println("\033[34Microeconomía \n 7am- 9am");
+        System.out.println("\\033[32Programación  \n 9am- 11am");
+        System.out.println("\\033[30Calculo II    \n11am-1pm");
+        System.out.println("\\033[33Estadistica   \n 2pm-4pm");
+        System.out.println("\\033[34Física II     \n 4pm-6pm");
+    }
+}
